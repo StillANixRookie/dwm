b31c5d6e0179d5f91caea7d07200f10d9062e1b8
StillANixRookie <gvales2831997@gmail.com>
2020-06-28 22:26:48 +0000

    Sun 28 Jun 2020 22:26:48 GMT

--- config.h
+++ config.h
@@ -1,21 +1,24 @@
 // See LICENSE file for copyright and license details.
 
 // appearance
-static unsigned int borderpx = 1;        // border pixel of windows
-static unsigned int snap     = 32;       // snap pixel
-static unsigned int minwsz   = 20;       /* Minimal heigt of a client for smfact */
-static int swallowfloating   = 0;        /* 1 means swallow floating windows by default */
-static int rmaster           = 1;        /* 1 means master-area is initially on the right */
-static int showbar           = 0;        // 0 means no bar
-static int topbar            = 1;        // 0 means bottom bar
-static char *fonts[]         = { "monospace:size=10" };
-static char dmenufont[]      = "monospace 10";
-static char col_gray1[]      = "#222222";
-static char col_gray2[]      = "#444444";
-static char col_gray3[]      = "#bbbbbb";
-static char col_gray4[]      = "#eeeeee";
-static char col_cyan[]       = "#005577";
-static char *colors[][3]     = {
+static const unsigned int borderpx = 1;        // border pixel of windows
+static const unsigned int snap     = 32;       // snap pixel
+static const unsigned int gappih   = 0;       /* horiz inner gap between windows */
+static const unsigned int gappiv   = 0;       /* vert inner gap between windows */
+static const unsigned int gappoh   = 20;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov   = 20;       /* vert outer gap between windows and screen edge */
+static const int smartgaps         = 0;        /* 1 means no outer gap when there is only one window */
+static const int swallowfloating   = 0;        /* 1 means swallow floating windows by default */
+static const int showbar           = 0;        // 0 means no bar
+static const int topbar            = 1;        // 0 means bottom bar
+static const char *fonts[]         = { "monospace:size=10" };
+static const char dmenufont[]      = "Iosevka 15";
+static const char col_gray1[]      = "#222222";
+static const char col_gray2[]      = "#444444";
+static const char col_gray3[]      = "#bbbbbb";
+static const char col_gray4[]      = "#eeeeee";
+static const char col_cyan[]       = "#005577";
+static const char *colors[][3]     = {
 	//               fg         bg         border  
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
@@ -56,16 +59,16 @@ static const Rule rules[] = {
 };
 
 // layouts
-static const float mfact       = 0.35; // factor of master area size [0.05..0.95]
-static const float smfact      = 0.00; // factor of tiled clients [0.00..0.95]
+static const float mfact       = 0.40; // factor of master area size [0.05..0.95]
+//static const float smfact      = 0.00; // factor of tiled clients [0.00..0.95]
 static const int   nmaster     = 1;    // number of clients in master area
 static const int   resizehints = 1;    // 1 means respect size hints in tiled resizals
 
 static const Layout layouts[] = {
 	// symbol     arrange function
+	{ "|M|",      centeredmaster },
 	{ "[]=",      tile },    // first entry is default
 	{ "[M]",      monocle },
-	{ "|M|",      centeredmaster },
 	{ ">M>",      centeredfloatingmaster },
 	{ "><>",      NULL },    // no layout function means floating behavior
 };
@@ -73,9 +76,9 @@ static const Layout layouts[] = {
 // key definitions
 #define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY,                       KEY,      comboview,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      combotag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 // helper for spawning shell commands in the pre dwm-5.0 fashion
@@ -83,63 +86,80 @@ static const Layout layouts[] = {
 
 // commands
 static char dmenumon[2] = "0"; // component of dmenucmd, manipulated in spawn()
-static const char *dmenucmd[] = { "dmenu_run",
-	"-f", dmenufont, "-m 0 -l 10 -L mc",
+static const char *dmenucmd[] = { "dmrap",
+	"-a lightgrn", "-f", dmenufont, "-m 0 -l 10 -L mc",
 	NULL
 	};
 static const char *termcmd[]  = { "st", NULL };
 
 #include "movestack.c"
 static Key keys[] = {
-	// TAGBINDS  key  argument
-	TAGKEYS(    XK_1,   0)
-	TAGKEYS(    XK_2,   1)
-	TAGKEYS(    XK_3,   2)
-	TAGKEYS(    XK_4,   3)
-	TAGKEYS(    XK_5,   4)
-	TAGKEYS(    XK_6,   5)
-	TAGKEYS(    XK_7,   6)
-	TAGKEYS(    XK_8,   7)
-	TAGKEYS(    XK_9,   8)
+	// TAGBINDS  key  tag
+	TAGKEYS(    XK_1, 0)
+	TAGKEYS(    XK_2, 1)
+	TAGKEYS(    XK_3, 2)
+	TAGKEYS(    XK_4, 3)
+	TAGKEYS(    XK_5, 4)
+	TAGKEYS(    XK_6, 5)
+	TAGKEYS(    XK_7, 6)
+	TAGKEYS(    XK_8, 7)
+	TAGKEYS(    XK_9, 8)
+	// VANITY GAPS CTRL
+	{ MODKEY|Mod4Mask,              XK_h,      incrgaps,       {.i = +1 } },
+	{ MODKEY|Mod4Mask,              XK_l,      incrgaps,       {.i = -1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
+	{ MODKEY|Mod4Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
+	{ MODKEY|Mod4Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
+	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
+	{ MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
+	{ MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
+	{ MODKEY|Mod4Mask,              XK_y,      incrohgaps,     {.i = +1 } },
+	{ MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
 
 	// modifier         key        function        argument
+	{ MODKEY,           XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY,           XK_Tab,    comboview,           {0} },
+	{ MODKEY,           XK_q,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,           XK_w,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,           XK_e,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,           XK_r,      setlayout,      {.v = &layouts[3]} },
+//	{ MODKEY,           XK_t,      togglermaster,  {0} },
+	{ MODKEY,           XK_y,      incnmaster,     {.i = +1 } },
+	{ MODKEY,           XK_u,      incnmaster,     {.i = -1 } },
 	{ MODKEY,           XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,           XK_b,      togglebar,      {0} },
+	{ MODKEY,           XK_Return, zoom,           {0} },
+	{ MODKEY,           XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,           XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,           XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,           XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,           XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,           XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,           XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,           XK_x,      transfer,       {0} },
-	{ MODKEY,           XK_Return, zoom,           {0} },
-	{ MODKEY,           XK_Tab,    view,           {0} },
-	{ MODKEY,           XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,           XK_m,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,           XK_u,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,           XK_o,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,           XK_b,      togglebar,      {0} },
 //	{ MODKEY,           XK_f,      setlayout,      {.v = &layouts[4]} },
-	{ MODKEY,           XK_space,  setlayout,      {0} },
-	{ MODKEY,           XK_r,      togglermaster,  {0} },
-	{ MODKEY,           XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY,           XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,           XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY,           XK_space,  setlayout,      {0} },
 
-	{ MODKEY|ShiftMask, XK_h,      setsmfact,      {.f = +0.05} },
-	{ MODKEY|ShiftMask, XK_l,      setsmfact,      {.f = -0.05} },
+	{ MODKEY|ShiftMask, XK_0,      combotag,            {.ui = ~0 } },
 	{ MODKEY|ShiftMask, XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask, XK_f,      togglefullscr,  {0} },
+//	{ MODKEY|ShiftMask, XK_h,      setsmfact,      {.f = +0.05} },
 	{ MODKEY|ShiftMask, XK_j,      movestack,      {.i = +1 } },
 	{ MODKEY|ShiftMask, XK_k,      movestack,      {.i = -1 } },
+//	{ MODKEY|ShiftMask, XK_l,      setsmfact,      {.f = -0.05} },
 	{ MODKEY|ShiftMask, XK_c,      killclient,     {0} },
-	{ MODKEY|ShiftMask, XK_space,  togglefloating, {0} },
-	{ MODKEY|ShiftMask, XK_f,      togglefullscr,  {0} },
-	{ MODKEY|ShiftMask, XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY|ShiftMask, XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask, XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask, XK_space,  togglefloating, {0} },
 
-	{ MODKEY|ControlMask, XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask, XK_q,     quit,           {0} },
 
-	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
+	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} },
 };
 
 // button definitions
@@ -153,9 +173,9 @@ static Button buttons[] = {
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkTagBar,            0,              Button1,        comboview,      {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        combotag,       {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 
